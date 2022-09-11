549fc9be57561ad83e7fb1e7f2a9a5d3ef9df5b3
jelenaa25 <beganovicjelena@gmail.com>
2022-09-11 15:03:28 +0200

    omogucen prikaz svih komada nakita iz baze

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="css/style.css" />
     <title>Dobro došli!</title>
  
