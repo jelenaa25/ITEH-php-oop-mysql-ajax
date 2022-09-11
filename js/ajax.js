6881814ca0c9be43add9a5ae30ff55c3bd58ddae
jelenaa25 <beganovicjelena@gmail.com>
2022-09-11 15:12:12 +0200

    omoguceno brisanje nakita iz baze

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+function obrisinakit(deleteid){//2
+
+
+    request = $.ajax({  
+        url: 'handler/delete.php',  
+        type: 'post', 
+        data: {deleteid:deleteid},
+
+
+        success: function(data, status){
+            location.reload(true);
+            alert("Uspesno obrisano!");
+        }
+
+
+    });
+
+
+
+}
